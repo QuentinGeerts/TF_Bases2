f6d621c5d2d4f965cd731089d52b5737ed8dd4ba
QuentinGeerts <quentin.geerts@bstorm.be>
2022-01-25 11:19:40 +0100

    first commit

---
+++ Demo/introduction/script.js
@@ -0,0 +1,12 @@
+// Commentaire simple ligne en JS
+/*
+  Un commentaire
+  sur
+  plusieurs
+  lignes
+*/
+//  CTRL + :
+
+console.log('Bonjour')
+console.warn('Bonjour');
+console.error('Bonjour');
