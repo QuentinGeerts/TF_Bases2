2423ce56032e9b0053930b92b1faf892d55cafc7
QuentinGeerts <quentin.geerts@bstorm.be>
2022-01-26 10:36:36 +0100

    Operateur + prompt

--- Demo/0_Introduction/script.js
+++ Demo/0_Introduction/script.js
@@ -10,3 +10,11 @@
 console.log('Bonjour')
 console.warn('Bonjour');
 console.error('Bonjour');
+
+
+// alert("Affiche le message dans la boite de dialogue")
+
+let age = prompt("Entrez votre age", 20)
+
+console.log('age :>> ', age);
+console.log(typeof age);
