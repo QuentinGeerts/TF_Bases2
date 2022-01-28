56df914dfd33508671cf40c92d688e3697e48242
QuentinGeerts <quentin.geerts@bstorm.be>
2022-01-28 16:48:48 +0100

    Correction exercice 02

---
+++ DOM/Exercices/exo02-bonus/script.js
@@ -0,0 +1,22 @@
+// On récupère nos éléments existants
+const btn = document.getElementById('btn')
+const liste = document.getElementById('liste')
+const itemName= document.getElementById('itemName')
+
+console.log('itemName :>> ', itemName.value);
+
+// On ajoute un comportement au clique du bouton
+btn.onclick = addItem
+
+// Fonction qui permet d'ajouter un item à la liste
+function addItem() {
+
+  // On crée un item de type "li"
+  const item = document.createElement('li')
+
+  // On donne un texte au li
+  item.innerText = itemName.value
+
+  // On ajoute le li à la liste (ul)
+  liste.appendChild(item)
+}
